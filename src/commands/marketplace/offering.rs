c754f78dcbef68c4d9b5333bb46c2ea620f6d887
Ilja Livenson <ilja@opennodecloud.com>
2026-09-12 09:32:26 +0000

    chore: regenerate CLI command surface from OpenAPI schema

--- src/commands/marketplace/offering.rs
+++ src/commands/marketplace/offering.rs
@@ -82,6 +82,7 @@ pub struct OfferingListArgs {
             "backend_id",
             "backend_metadata",
             "billable",
+            "billing_mode_components",
             "billing_period_applies",
             "billing_type_classification",
             "can_update_integration",
